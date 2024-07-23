3134d1a3aac226713abdb4ae63b983f62207915a
Fabio Pedrini <fabio@pedrini.io>
2024-07-23 23:17:03 +0200

    Add group_expand api to project model to see all kanban columns in projects view

--- models/project.py
+++ models/project.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class Project(models.Model):
@@ -11,7 +11,11 @@ class Project(models.Model):
         ('on_track', 'On Track'),
         ('at_risk', 'At Risk'),
         ('delayed', 'Delayed'),
-    ], string='Status', default='on_track')
+    ], string='Status', default='on_track', group_expand='_group_expand_status')
     issue_ids = fields.One2many('pt.issue', 'project_id', string='Issues')
     milestone_ids = fields.One2many('pt.project.milestone', 'project_id', string='Milestones')
     issue_tag_ids = fields.One2many('pt.project.issue.tag', 'project_id', string='Issues Tags')
+
+    @api.model
+    def _group_expand_status(self, statuses, domain, order):
+        return [key for key, val in type(self).status.selection]
